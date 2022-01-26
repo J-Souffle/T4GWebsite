bf8d59f972ff2b427d1f33fc7ab4f88f80e08197
Liam McKane <lmckane04@gmail.com>
2022-01-25 19:59:05 -0500

    slight footer changes

--- _includes/index.php
+++ _includes/index.php
@@ -3,11 +3,11 @@
     <div class="container">
         <form class="contact-form" action="contactform.php" method="post">
           <!-- <label for="name">Your Name:</label> -->
-          <input type="text" id="name" name="your name" placeholder="Name">
+          <input type="form-text" id="name" name="your name" placeholder="Name">
           <!-- <label for="email">Email:</label> -->
-          <input type="text" id="email" name="email" placeholder="Email">
+          <input type="form-text" id="email" name="email" placeholder="Email">
           <!-- <label for="subject">Subject:</label> -->
-          <input type="text" id="subject" name="subject" placeholder="Subject">
+          <input type="form-text" id="subject" name="subject" placeholder="Subject">
           <textarea id="message" name="subject" placeholder="Message" style="height:10rem"></textarea>
           <button type="submit" name="Submit">Submit</button>
         </form>
