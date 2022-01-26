b7068dc06877ee3847d0289b393a17ac965eaf43
Liam McKane <lmckane04@gmail.com>
2022-01-25 21:11:13 -0500

    get forms

--- _includes/index.php
+++ _includes/index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <section id="contact">
     <div class="container">
-        <form class="contact-form" action="contactform.php" method="post">
+        <form class="contact-form" action="https://getform.io/f/87f32953-f3f3-422f-b962-cb6734a7ca15" method="post">
           <!-- <label for="name">Your Name:</label> -->
           <input type="form-text" id="name" name="your name" placeholder="Name">
           <!-- <label for="email">Email:</label> -->
           <input type="form-text" id="email" name="email" placeholder="Email">
           <!-- <label for="subject">Subject:</label> -->
           <input type="form-text" id="subject" name="subject" placeholder="Subject">
-          <textarea id="message" name="subject" placeholder="Message" style="height:10rem"></textarea>
-          <button type="submit" name="Submit">Submit</button>
+          <textarea id="message" name="message" placeholder="Message" style="height:10rem"></textarea>
+          <button type="submit" class="Submit">Submit</button>
         </form>
-        <input type = "hidden" name="_next" value="thanks.html"/>
     </div>
 <section>
